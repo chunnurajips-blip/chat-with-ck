61f7885a01a4c49c7df5578853884af8e123e317
chunnurajips-blip <chunnurajips@gmail.com>
2026-06-28 16:55:37 +0530

    signup controller added

---
+++ Backend/src/lib/utlis.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  // create a token fo the user
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    httpOnly: true, // prvent to xss attack
+    sameSite: "strict", // csrf attacks
+    secure: process.env.NODE_ENV == "development" ? false : true,
+  });
+
+  return token;
+};
