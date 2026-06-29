ea1755cb9e770c7049d5ab35861170099ef09ac5
chunnurajips-blip <chunnurajips@gmail.com>
2026-06-29 20:05:47 +0530

    update project

--- Backend/src/Routes/auth.routes.js
+++ Backend/src/Routes/auth.routes.js
@@ -1,15 +1,10 @@
 import express from "express";
-<<<<<<< HEAD
-import { signup, login, logout } from "../controllers/auth.controllers.js";
-=======
 import { signup, login, logout } from "../controllers/auth.controller.js";
->>>>>>> 29814d8 (update project)
+
 const router = express.Router();
 
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
 
 export default router;
--- Backend/src/lib/utlis.js
+++ Backend/src/lib/utlis.js
@@ -1,40 +1,24 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const { JWT_SECRET, NODE_ENV } = process.env;
-<<<<<<< HEAD
-  if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET is not configured");
-  }
-  // create a token fo the user
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // ms
-    httpOnly: true, // prvent to xss attack
-    sameSite: "strict", // csrf attacks
-    secure: process.env.NODE_ENV == "development" ? false : true,
-=======
-
+  const { JWT_SECRET } = process.env;
   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not configured");
   }
 
-  // Create JWT token
   const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
 
-  // Store token in cookie
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    httpOnly: true, // Prevent XSS attacks
-    sameSite: "strict", // Prevent CSRF attacks
-    secure: NODE_ENV === "production", // true only in production
->>>>>>> 29814d8 (update project)
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks: cross-site scripting
+    sameSite: "strict", // CSRF attacks
+    secure: process.env.NODE_ENV === "development" ? false : true,
   });
 
   return token;
 };
+
+// http://localhost
+// https://dsmakmk.com
