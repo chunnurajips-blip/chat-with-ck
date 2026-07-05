897d571f8fea482185bada6ec6b98f7b1cb49ede
chunnurajips-blip <chunnurajips@gmail.com>
2026-07-06 01:11:10 +0530

    update project

--- Backend/src/Routes/auth.routes.js
+++ Backend/src/Routes/auth.routes.js
@@ -5,21 +5,22 @@ import {
   updateProfile,
   logout,
 } from "../controllers/auth.controller.js";
+
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { arcjectProtection } from "../middleware/arcject.middleware.js";
 
 const router = express.Router();
 
-router.use(arcjectProtection, protectRoute);
-
-router.post("/signup", signup);
-router.post("/login", login);
+// Public routes (no JWT required)
+router.post("/signup", arcjectProtection, signup);
+router.post("/login", arcjectProtection, login);
 router.post("/logout", logout);
 
+// Protected routes (JWT required)
 router.put("/update-profile", protectRoute, updateProfile);
 
-router.get("/check", protectRoute, (req, res) =>
-  res.status(200).json(req.user),
-);
+router.get("/check", protectRoute, (req, res) => {
+  res.status(200).json(req.user);
+});
 
 export default router;
