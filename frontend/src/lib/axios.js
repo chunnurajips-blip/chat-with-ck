011dd1161d71cf3fce71aeb18a9f0c8454dc9c77
chunnurajips-blip <chunnurajips@gmail.com>
2026-07-06 01:19:40 +0530

    update project

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "https://chat-with-ck.onrender.com",
+      : "https://chat-with-ck.onrender.com/api",
 
   withCredentials: true,
 });
