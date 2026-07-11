6434018964a1cdd934b5870dc339f082982ca822
chunnurajips-blip <chunnurajips@gmail.com>
2026-07-11 15:11:47 +0530

    update project

--- Backend/src/store/useAuthStore.js
+++ Backend/src/store/useAuthStore.js
@@ -4,8 +4,9 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
-
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://chat-with-ck.onrender.com";
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isCheckingAuth: true,
