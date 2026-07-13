fe0fd62fe9650d88a0982f782007d78543b69146
chunnurajips-blip <chunnurajips@gmail.com>
2026-07-13 20:10:24 +0530

    check cookies

--- Backend/src/lib/utlis.js
+++ Backend/src/lib/utlis.js
@@ -2,6 +2,9 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
+  console.log("NODE_ENV =", ENV.NODE_ENV);
+  console.log("CLIENT_URL =", ENV.CLIENT_URL);
+
   const { JWT_SECRET } = ENV;
   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not configured");
@@ -11,6 +14,10 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
+  console.log({
+    secure: ENV.NODE_ENV !== "development",
+    sameSite: ENV.NODE_ENV === "development" ? "lax" : "none",
+  });
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
@@ -20,6 +27,3 @@ export const generateToken = (userId, res) => {
 
   return token;
 };
-
-// http://localhost
-// https://dsmakmk.com
