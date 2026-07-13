11fd9970ad39c379b2cda60d34a3bb30b22f4251
chunnurajips-blip <chunnurajips@gmail.com>
2026-07-13 20:20:14 +0530

    fix cookies

--- Backend/src/lib/utlis.js
+++ Backend/src/lib/utlis.js
@@ -2,9 +2,6 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-  console.log("NODE_ENV =", ENV.NODE_ENV);
-  console.log("CLIENT_URL =", ENV.CLIENT_URL);
-
   const { JWT_SECRET } = ENV;
   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not configured");
@@ -14,10 +11,6 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
-  console.log({
-    secure: ENV.NODE_ENV !== "development",
-    sameSite: ENV.NODE_ENV === "development" ? "lax" : "none",
-  });
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
